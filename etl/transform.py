8d5b1eeec72aabed3b167baf8a171e5d1079c978
Vinicius Moraes <vini.moraes1206@gmail.com>
2025-08-19 21:08:47 -0300

    funcoes de tratamento

--- etl/transform.py
+++ etl/transform.py
@@ -9,7 +9,7 @@ def padronizar_nomes_colunas(df: pd.DataFrame) -> pd.DataFrame:
         .str.normalize('NFKD')        # remove acentos
         .str.encode('ascii', errors='ignore')
         .str.decode('utf-8')
-        .str.upper()
+        .str.lower()
     )
     return df
 
@@ -18,8 +18,8 @@ def limpar_espacos(df: pd.DataFrame) -> pd.DataFrame:
     """
     Limpa a coluna 'Descrição': remove espaços e prefixos como '(=)', '(-)' etc.
     """
-    df["DESCRICAO"] = (
-        df["DESCRICAO"]
+    df["descricao"] = (
+        df["descricao"]
         .astype(str)
         .str.strip()
         .str.replace(r"^[\(\)=\-\+]+\s*", "", regex=True)
@@ -31,6 +31,11 @@ def converter_meses_para_float(df: pd.DataFrame) -> pd.DataFrame:
     """
     Converte todas as colunas, exceto 'Descrição', para float.
     """
-    colunas_meses = df.columns.drop("Descrição")
+    colunas_meses = df.columns.drop("descricao")
     df[colunas_meses] = df[colunas_meses].apply(pd.to_numeric, errors="coerce")
+    return df
+
+def remover_coluna_total(df: pd.DataFrame) -> pd.DataFrame:
+    if "total" in df.columns:
+        df = df.drop(columns=["total"])
     return df
